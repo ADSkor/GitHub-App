98a478c0c9f23afb421e0a90e912f9dd8a185d47
Александр Скороткин <killko_judo@mail.ru>
2018-11-30 08:51:29 +0700

    Initial Commit

---
+++ RepoGit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RepoGit
+//
+//  Created by Aleksandr Skorotkin on 30/11/2018.
+//  Copyright © 2018 Aleksandr Skorotkin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
